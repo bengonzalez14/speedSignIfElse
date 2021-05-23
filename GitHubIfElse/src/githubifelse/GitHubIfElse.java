d886c2de4f6a3c352b230e4b21dd1a691a7af961
bengonzalez14 <55911394+bengonzalez14@users.noreply.github.com>
2021-05-23 10:15:48 +0100

    Add files via upload

---
+++ GitHubIfElse/src/githubifelse/GitHubIfElse.java
@@ -0,0 +1,20 @@
+
+package githubifelse;
+
+
+public class GitHubIfElse {
+
+    public static void main(String[] args) {
+       
+        int Speed = 30;
+        
+        if (Speed<=30){
+        System.out.println("thank you");
+        }
+                
+        else{
+        System.out.println("slow down");}
+        
+    }
+    
+}
